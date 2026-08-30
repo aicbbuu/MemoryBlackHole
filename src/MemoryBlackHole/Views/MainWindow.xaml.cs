679cbd2245cc96653ec25273cf2c147eb22b1e6a
aicbbuu <aicbbuu@users.noreply.github.com>
2026-08-29 22:24:12 -0400

    build(v3.0.0): 补 using System.Windows.Threading 修复 DispatcherTimer CS0246

--- src/MemoryBlackHole/Views/MainWindow.xaml.cs
+++ src/MemoryBlackHole/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Effects;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using MemoryBlackHole.Models;
 using MemoryBlackHole.Services;
 
